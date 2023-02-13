0f64afe453e41f5f74a9e5e2e8e5ccebbf49f552
Haipham2002 <71917032+Haipham2002@users.noreply.github.com>
2023-02-13 23:23:41 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,29 +6,6 @@
 #include <random>
 
 void printHelp() {
-//    const char *helptext = "usage: console_sodoku [OPTIONS] \n\n"
-//                           "Play Sudoku in the terminal.\n\n"
-//                           "Optional args:\n"
-//                           "-h, --help       View this page\n"
-//                           "-w,  --wasd      Display wasd instead of hjkl\n"
-//                           "--opensudoku     Load a puzzle from an opensudoku file\n"
-//                           "--sdm            Load a puzzle from an smd file\n"
-//                           "INTEGER          Number of squares to leave empty\n\n"
-//                           "Move with hjkl, wasd or the arrow keys.\n"
-//                           "Enter pencil mode by pressing 'p'.\n"
-//                           "Enter insert mode by pressing 'i'.\n"
-//                           "Go to a specific square by pressing 'g' followed by the column then row.\n"
-//                           "Check the board for mistakes by pressing 'c'.\n"
-//                           "Quit the game by pressing 'q'.\n"
-//                           "To highlight any occurence of a number insert the number in any mode or\n"
-//                           "press spacebar on a prefilled box.\n"
-//                           "Hint: pencilmarks do not show up on filled in boxes, use this to highlight.\n\n"
-//                           "For information on Sudoku puzzles see the wikipedia entry:\n"
-//                           "https://en.wikipedia.org/wiki/Sudoku\n\n"
-//                           "See the readme for more information.\n"
-//                           "The readme can be read online at:\n"
-//                           "https://github.com/flyingpeakock/Console_sudoku/blob/master/README.md\n"
-//                           "https://philipj.ydns.eu/stagit/Console_sudoku/file/README.md.html\n";
     printf("usage: console_sodoku [OPTIONS] \n\n"
                            "Play Sudoku in the terminal.\n\n"
                            "Optional args:\n"
@@ -96,18 +73,6 @@ int main(int argc, char *argv[]) {
        	if (atoi(argv[i]) > 0) {
            	gen = new Generator(atoi(argv[i]));
        	}
-// 		if (strcmp(argv[i], "-easy") == 0) {
-// 			gen = new Generator(49);
-// 		}
-// 		if (strcmp(argv[i], "-medium") == 0) {
-// 			gen = new Generator(35);
-// 		}
-// 		if (strcmp(argv[i], "-hard") == 0) {
-// 			gen = new Generator(28);
-// 		}
-// 		if (strcmp(argv[i], "-master") == 0) {
-// 			gen = new Generator(21);
-// 		}
         if (strcmp(argv[i], "-w") == 0 || strcmp(argv[i], "--wasd") == 0) {
             strcpy(navKeys, "aswd");
         }
